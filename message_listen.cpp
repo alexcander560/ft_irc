37cc0aab496a1479cd73dd229b4c7fde1e6e402a
imora <imora@student.21-school.ru>
2021-11-14 10:55:54 +0300

    Create socket, set options and use bind() - OK

--- message_listen.cpp
+++ message_listen.cpp
@@ -30,17 +30,13 @@ static int	create_socket()
 			fatal("The system file table is full.");
 			break ;
 		case ENOBUFS:
-			fatal("Insufficient buffer space is available.  The socket"
-				  "cannot be created until sufficient resources are"
-				  "freed.");
+			fatal("Insufficient buffer space is available.  The socket   cannot be created until sufficient resources are   freed.");
 			break ;
 		case ENOMEM:
-			fatal("Insufficient memory was available to fulfill the"
-				  "request.");
+			fatal("Insufficient memory was available to fulfill the   request.");
 			break ;
 		case EPROTONOSUPPORT:
-			fatal("The protocol type or the specified protocol is not"
-				  "supported within this domain.");
+			fatal("The protocol type or the specified protocol is not   supported within this domain.");
 			break ;
 		case EPROTOTYPE:
 			fatal("The socket type is not supported by the protocol.");
@@ -66,8 +62,7 @@ static void	set_socket_options(const int socket_fd)
 			fatal("The argument option_value is out of bounds.");
 			break ;
 		case EISCONN:
-			fatal("Socket is already connected and a specified option"
-				  "cannot be set while this is the case.");
+			fatal("Socket is already connected and a specified option   cannot be set while this is the case.");
 			break ;
 		case EINVAL:
 			fatal("The socket has been shut down.");
@@ -79,14 +74,58 @@ static void	set_socket_options(const int socket_fd)
 
 static void	bind_socket(const int socket_fd, const int port)
 {
+	int	bind_value;
 	struct sockaddr_in address; //For IPv4 instead IPv6 (sockaddr_in6);
 
 	address.sin_family = AF_INET;
 	address.sin_port = port;
 	address.sin_addr.s_addr = INADDR_ANY;
 
-	bind(socket_fd, &address, sizeof(address));
-	if ()
+	bind_value = bind(socket_fd, reinterpret_cast<struct sockaddr *>(&address), sizeof(address));
+	if (bind_value != -1)
+		return ;
+	switch (errno)
+	{
+		case EACCES:
+			fatal("The address is protected, and the user is not the   superuser.");
+			break ;
+		case EADDRINUSE:
+			fatal("The port number was specified as zero in the socket address structure, but, upon attempting to bind to an ephemeral port, it was determined that all port numbers in the ephemeral port range are currently in use.");
+			break ;
+		case EBADF:
+			fatal("Socket FD is not a valid file descriptor.");
+			break ;
+		case EINVAL:
+			fatal("The socket is already bound to an address.");
+			break ;
+		case EADDRNOTAVAIL:
+			fatal("A nonexistent interface was requested or the requested address was not local.");
+			break ;
+		case EFAULT:
+			fatal("Address points outside the user's accessible address space.");
+			break ;
+		case ELOOP:
+			fatal("Too many symbolic links were encountered in resolving address.");
+			break ;
+		case ENAMETOOLONG:
+			fatal("Address is too long.");
+			break ;
+		case ENOENT:
+			fatal("A component in the directory prefix of the socket pathname does not exist.");
+			break ;
+		case ENOMEM:
+			fatal("Insufficient kernel memory was available.");
+			break ;
+		case ENOTDIR:
+			fatal("A component of the path prefix is not a directory.");
+			break ;
+		case EROFS:
+			fatal("The socket inode would reside on a read-only filesystem.");
+			break ;
+		default:
+			fatal("Unknown error.");
+			break ;
+	}
 }
 
 void	listen_messages(const int port)
