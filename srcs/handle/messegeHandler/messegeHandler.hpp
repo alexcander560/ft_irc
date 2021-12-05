e1d006c210de0db4adee6632a2fa1fe0d5b3506b
imora <imora@student.21-school.ru>
2021-12-05 13:47:08 +0300

    VERSION - 100%

--- srcs/handle/messegeHandler/messegeHandler.hpp
+++ srcs/handle/messegeHandler/messegeHandler.hpp
@@ -6,6 +6,7 @@
 //#include "../users/user.hpp"
 
 #define SERVER_NAME "DragonsCHAT"
+#define SERVER_VERSION "release (1.12.2)"
 
 class MassegeHandler {
 private:
@@ -258,13 +259,22 @@ private:
 //	vector< pair<int, string> > command_pong(pair<map<int, User>::iterator, bool> *res,vector< pair<int, string> > *message) {}
 //	vector< pair<int, string> > command_ison(pair<map<int, User>::iterator, bool> *res,vector< pair<int, string> > *message) {}
 //	vector< pair<int, string> > command_userhost(pair<map<int, User>::iterator, bool> *res,vector< pair<int, string> > *message) {}
-//	vector< pair<int, string> > command_version(pair<map<int, User>::iterator, bool> *res,vector< pair<int, string> > *message) {}
+	vector< pair<int, string> > command_version(pair<map<int, User>::iterator, bool> *res,vector< pair<int, string> > *message)
+	{
+		if (lenparam == 1 || (lenparam == 2 && param[1] == SERVER_NAME))
+			message->push_back(make_pair(id, getFrontLineRPL(SERVER_VERSION, RPL_VERSION) + "\n"));
+		else
+			debug(RED"[command_version] No such server"DEFAULT);
+		return (*message);
+	}
 //	vector< pair<int, string> > command_info(pair<map<int, User>::iterator, bool> *res,vector< pair<int, string> > *message) {}
 //	vector< pair<int, string> > command_admin(pair<map<int, User>::iterator, bool> *res,vector< pair<int, string> > *message) {}
 	vector< pair<int, string> > command_time(pair<map<int, User>::iterator, bool> *res,vector< pair<int, string> > *message)
 	{
 		if (lenparam == 1 || (lenparam == 2 && param[1] == SERVER_NAME))
-			message->push_back(make_pair(id, getFrontLineRPL(getCurrentTime(), RPL_TIME))); //TODO
+			message->push_back(make_pair(id, getFrontLineRPL(getCurrentTime(), RPL_TIME)));
+		else
+			debug(RED"[command_time] No such server"DEFAULT);
 		return (*message);
 	}
 public:
@@ -295,22 +305,23 @@ public:
 		commands["AWAY"] = &MassegeHandler::command_away;
 		commands["NOTICE"] = &MassegeHandler::command_notice;
 		commands["MODE"] = &MassegeHandler::command_mode;
+		commands["VERSION"] = &MassegeHandler::command_version;
 		commands["TIME"] = &MassegeHandler::command_time;
 	}
 	// Распечатка
 	void printMassege() {
 		string	temp = "";
-		debug("[handle_message] Cообщение получено {" + str_message + "}");
+		debug("[printMassege] Cообщение получено {" + str_message + "}");
 		for (int i = 0; i < lenparam; i++)
 			temp += "(" + param[i] + ") ";
 		if (DEBUG)
 		{
-			debug(clients->size() ? "[handle_message] Печать всех пользователей" : "[handle_message] Нет пользователей");
+			debug(clients->size() ? "[printMassege] Печать всех пользователей" : "[handle_message] Нет пользователей");
 			for (map<int, User>::iterator it1 = clients->begin(); it1 != clients->end(); it1++)
 				it1->second.printUser();
-			debug(lenparam ? "[handle_message] Аргументы: " + temp : RED"[handle_message] нет аргументов" DEFAULT);
+			debug(lenparam ? "[printMassege] Аргументы: " + temp : RED"[handle_message] нет аргументов" DEFAULT);
 		}
-		debug("[handle_message] =============================================================================");
+		debug("[printMassege] =============================================================================");
 	}
 	// Обработка
 	vector< pair<int, string> > message() {
