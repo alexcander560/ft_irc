14918667e1fc612a3a223c412c90cd4e45e6858c
imora <imora@student.21-school.ru>
2021-12-10 18:57:44 +0300

    OK

--- srcs/ircserv.cpp
+++ srcs/ircserv.cpp
@@ -1,8 +1,21 @@
 #include "listen/listen.hpp"
 
-#define PORT 42
-
-int	main(void)
+void	stop(std::string str)
 {
+	std::cerr << str << std::endl;
+	exit(EXIT_FAILURE);
+}
+
+int	main(int args_count, char **args)
+{
+	int	port;
+
+	if (args_count != 3)
+		stop("It's few to params");
+	port = std::atoi(args[2]);
+	if (port < 1024 || port > 49151)
+		stop("Wrong port");
+#define PORT port
+#define PASSWORD args[1]
 	listen_messages(PORT);
 }
